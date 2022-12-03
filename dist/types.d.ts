d769825174a472ac0013c8c8e295b7fdb77df19d
plondon <philip.n.london@gmail.com>
2022-12-03 15:15:08 -0500

    Build

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1,11 +1,13 @@
 export interface Map3InitConfig {
+    address?: string;
     anonKey: string;
+    assetId?: string;
     fiat?: string;
     generateDepositAddress: (asset?: string, network?: string, memoEnabled?: boolean) => Promise<{
         address: string;
         memo?: string;
     }>;
-    slug?: string;
+    networkCode?: string;
     theme?: 'dark' | 'light';
 }
 export class Map3 {
