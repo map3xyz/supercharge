f31db8f6db1efc5ec0bd8f3ca814080d4ff8f73c
plondon <philip.n.london@gmail.com>
2022-12-04 17:18:03 -0500

    Test: utils

---
+++ src/utils/utils.test.tsx
@@ -0,0 +1,13 @@
+import { toHex } from './toHex';
+
+describe('toHex', () => {
+  it('should convert a number to hex', () => {
+    expect(toHex(0)).toEqual('0x0');
+    expect(toHex(1)).toEqual('0x1');
+    expect(toHex(10)).toEqual('0xa');
+    expect(toHex(15)).toEqual('0xf');
+    expect(toHex(16)).toEqual('0x10');
+    expect(toHex(255)).toEqual('0xff');
+    expect(toHex(256)).toEqual('0x100');
+  });
+});
