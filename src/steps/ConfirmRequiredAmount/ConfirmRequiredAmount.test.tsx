b72180d8a43c4f7d7f877a92d449e4873bd6fe56
plondon <philip.n.london@gmail.com>
2023-01-31 12:54:36 -0500

    fix: only allow minor units

--- src/steps/ConfirmRequiredAmount/ConfirmRequiredAmount.test.tsx
+++ src/steps/ConfirmRequiredAmount/ConfirmRequiredAmount.test.tsx
@@ -1,5 +1,5 @@
 import { mockConfig } from '~/jest/__mocks__/mockConfig';
-import { act, fireEvent, render, screen } from '~/jest/test-utils';
+import { fireEvent, render, screen } from '~/jest/test-utils';
 
 import App from '../../App';
 import ConfirmRequiredAmount from '.';
@@ -28,42 +28,6 @@ describe('ConfirmRequiredAmount', () => {
     it('renders', async () => {
       expect(await screen.findByText('Acknowledge Amount')).toBeInTheDocument();
       expect((await screen.findAllByText(/0.001 ETH/))[0]).toBeInTheDocument();
-    });
-  });
-  describe('renders if amount is required and qr code selected', () => {
-    beforeEach(async () => {
-      render(
-        <App
-          config={{
-            ...mockConfig,
-            amount: '0.1',
-            generateDepositAddress: async (_asset, _network) => {
-              return { address: '0x0000000000000000000000000000000000000000' };
-            },
-            networkCode: 'ethereum',
-          }}
-          onClose={() => {}}
-        />
-      );
-
-      await screen.findByText('Loading...');
-      const qrCode = await screen.findByText('Show Address');
-      fireEvent.click(qrCode);
-    });
-    it('renders', async () => {
-      expect(await screen.findByText('Acknowledge Amount')).toBeInTheDocument();
-    });
-    it('displays qr code after user acknowledgment', async () => {
-      const acknowledgeCheck = await screen.findByTestId(
-        'acknowledge-checkbox'
-      );
-      await act(async () => {
-        fireEvent.click(acknowledgeCheck);
-        const button = await screen.findByText('Acknowledge Amount');
-        fireEvent.click(button);
-      });
-      const qrCodeMethod = await screen.findByTestId('show-address-method');
-      expect(qrCodeMethod).toBeInTheDocument();
     });
   });
 });
