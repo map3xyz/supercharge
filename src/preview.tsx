b44a9aee30d043c85e57e3fbc1bc9ce81dcfd628
plondon <philip.n.london@gmail.com>
2023-02-07 16:22:02 -0500

    fix: remove test callback

--- src/preview.tsx
+++ src/preview.tsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 
 import { initMap3Supercharge } from '.';
-import { wait } from './utils/wait';
 
 const container = document.getElementById('app');
 
@@ -13,17 +12,6 @@ root.render(
         const supercharge = initMap3Supercharge({
           anonKey: process.env.CONSOLE_ANON_KEY || '',
           options: {
-            callbacks: {
-              handleOrderFeeCalculation: async (asset, networkCode, amount) => {
-                await wait(1000);
-
-                return {
-                  fixedFee: 0.1,
-                  message: 'Lorem ipsum dolor ipsum dolor ipsum dolor',
-                  variableFee: 0.1,
-                };
-              },
-            },
             selection: {
               assetId: '6b562c23-d79f-4a34-a47f-cc7b28726821',
               paymentMethod: 'binance-pay',
