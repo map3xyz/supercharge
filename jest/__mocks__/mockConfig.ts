e69d13cef497abd193a6201f123ba7f109fb3c3e
plondon <philip.n.london@gmail.com>
2023-02-08 15:36:29 -0500

    fix: test errors, add dummy fallback anon key

--- jest/__mocks__/mockConfig.ts
+++ jest/__mocks__/mockConfig.ts
@@ -1,7 +1,9 @@
 import { Map3InitConfig } from '../../src';
 
 export const mockConfig: Map3InitConfig = {
-  anonKey: process.env.CONSOLE_ANON_KEY || '',
+  anonKey:
+    process.env.CONSOLE_ANON_KEY ||
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjb25zb2xlIiwib3JnX2lkIjoiYzljNDczMzYtNWM5MS00MDM0LWIyYTgtMGI1NzA5ZTAwMGI1Iiwicm9sZXMiOlsiYW5vbnltb3VzIl0sImlhdCI6MTY3NTg4ODUwOCwiZXhwIjoxNzA3NDI0NTA4fQ.GzuXjFzSVkE3L-LlhtvpXa3aIi48rvHgMY3hw6lS8KU',
   options: {
     callbacks: {
       onAddressRequested: () => {
