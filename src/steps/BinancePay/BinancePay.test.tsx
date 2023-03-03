246a63a46d55eca43ca1330f12bcda5a8edd0888
plondon <philip.n.london@gmail.com>
2023-03-03 16:05:44 -0500

    test(binance pay): add tests for binance pay flow

---
+++ src/steps/BinancePay/BinancePay.test.tsx
@@ -0,0 +1,113 @@
+import * as reactDeviceDetect from 'react-device-detect';
+
+import { mockConfig } from '~/jest/__mocks__/mockConfig';
+import { act, fireEvent, render, screen } from '~/jest/test-utils';
+
+import App from '../../App';
+import BinancePay from '.';
+
+describe('BinancePay', () => {
+  describe('displays binance pay qr code on desktop', () => {
+    beforeEach(async () => {
+      render(
+        <App
+          config={{
+            ...mockConfig,
+            options: {
+              callbacks: {
+                // @ts-ignore
+                handleOrderFeeCalculation: () => ({
+                  fixedFee: 0.1,
+                  variableFee: 0.01,
+                }),
+                onAddressRequested: async (_asset, _network) => {
+                  return {
+                    address: '0x0000000000000000000000000000000000000000',
+                  };
+                },
+              },
+              selection: {
+                amount: '100000000',
+                assetId: 'elon123',
+              },
+            },
+          }}
+          onClose={() => {}}
+        />
+      );
+
+      await screen.findByText('Loading...');
+      const binancePay = await screen.findByText('Binance Pay');
+      fireEvent.click(binancePay);
+    });
+    it('renders', async () => {
+      expect(await screen.findByText('Enter Amount')).toBeInTheDocument();
+      const button = await screen.findByTestId('binance-pay-button');
+      await act(() => {
+        fireEvent.click(button);
+      });
+      await screen.findByText(/Receive Amount/);
+      const button2 = await screen.findByTestId('binance-pay-button');
+      await act(() => {
+        fireEvent.click(button2);
+      });
+      const payViaBinance = await screen.findByText('Pay via Binance');
+      expect(payViaBinance).toBeInTheDocument();
+    });
+  });
+  describe('mobile', () => {
+    beforeEach(async () => {
+      render(
+        <App
+          config={{
+            ...mockConfig,
+            options: {
+              callbacks: {
+                // @ts-ignore
+                handleOrderFeeCalculation: () => ({
+                  fixedFee: 0.1,
+                  variableFee: 0.01,
+                }),
+                onAddressRequested: async (_asset, _network) => {
+                  return {
+                    address: '0x0000000000000000000000000000000000000000',
+                  };
+                },
+              },
+              selection: {
+                amount: '100000000',
+                assetId: 'elon123',
+              },
+            },
+          }}
+          onClose={() => {}}
+        />
+      );
+
+      await screen.findByText('Loading...');
+      const binancePay = await screen.findByText('Binance Pay');
+      fireEvent.click(binancePay);
+    });
+    it('creates an order', async () => {
+      Object.defineProperties(reactDeviceDetect, {
+        isMobile: { get: () => true },
+      });
+
+      expect(await screen.findByText('Enter Amount')).toBeInTheDocument();
+      const button = await screen.findByTestId('binance-pay-button');
+      await act(() => {
+        fireEvent.click(button);
+      });
+      await screen.findByText(/Receive Amount/);
+      const button2 = await screen.findByTestId('binance-pay-button');
+      await act(() => {
+        fireEvent.click(button2);
+      });
+    });
+  });
+});
+
+describe('ConfirmRequireAmount Error', () => {
+  render(<BinancePay />);
+  expect(true).toBe(true);
+});
