f86e8b3167cedd6cc2952d5dd8a4dd6c08df2768
plondon <philip.n.london@gmail.com>
2023-03-03 09:30:52 -0500

    fix(expiration): add tooltip

--- src/components/CountdownTimer/index.tsx
+++ src/components/CountdownTimer/index.tsx
@@ -8,38 +8,40 @@ const CountdownTimer: React.FC<Props> = () => {
   const [seconds, setSeconds] = useState(remainingTime);
 
   const countdown = () => {
+    if (seconds <= 0) {
+      return;
+    }
     setSeconds((prev) => prev - 1);
   };
 
   useEffect(() => {
-    if (seconds < 0) {
-      return;
-    }
     countdown();
     const interval = setInterval(countdown, 1000);
 
     return () => clearInterval(interval);
   }, []);
 
-  return seconds >= 0 ? (
-    <svg
-      className="h-7 w-7 stroke-accent"
-      style={{ transform: 'rotateY(-180deg) rotateZ(-90deg)' }}
-    >
-      <circle
-        cx="50%"
-        cy="50%"
-        r="10"
-        style={{
-          animation: `countdown ${remainingTime}s linear`,
-          fill: 'none',
-          strokeDasharray: '63px',
-          strokeDashoffset: '0px',
-          strokeLinecap: 'round',
-          strokeWidth: '2px',
-        }}
-      ></circle>
-    </svg>
+  return seconds > 0 ? (
+    <span aria-label={`${seconds}s remaining.`} className="hint--left">
+      <svg
+        className="h-7 w-7 scale-75 stroke-accent"
+        style={{ transform: 'rotateY(-180deg) rotateZ(-90deg)' }}
+      >
+        <circle
+          cx="50%"
+          cy="50%"
+          r="10"
+          style={{
+            animation: `countdown ${remainingTime}s linear`,
+            fill: 'none',
+            strokeDasharray: `${Math.PI * 20}px`,
+            strokeDashoffset: '0px',
+            strokeLinecap: 'round',
+            strokeWidth: '2px',
+          }}
+        ></circle>
+      </svg>
+    </span>
   ) : (
     <span className="text-xs text-red-600">Expired</span>
   );
