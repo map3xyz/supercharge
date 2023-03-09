62cd023dcc1b6e813db1ce927f81dc1e5ec73030
plondon <philip.n.london@gmail.com>
2023-03-09 09:50:17 -0500

    fix(bridge): move progress logic to hook, dont update countdown and state if completed

--- src/utils/iso8601.ts
+++ src/utils/iso8601.ts
@@ -1,8 +1,11 @@
-export const iso8601ToString = (iso8601: string, offset?: number) => {
+export const iso8601ToDate = (iso8601: string, offset?: number) => {
   const tzoffset = (offset || new Date(iso8601).getTimezoneOffset()) * 60000;
-  const localISOTime = new Date(
-    new Date(iso8601).getTime() - tzoffset
-  ).toLocaleString();
+  const localISODate = new Date(new Date(iso8601).getTime() - tzoffset);
 
-  return localISOTime;
+  return localISODate;
+};
+
+export const iso8601ToString = (iso8601: string, offset?: number) => {
+  const date = iso8601ToDate(iso8601, offset);
+  return date.toLocaleString();
 };
